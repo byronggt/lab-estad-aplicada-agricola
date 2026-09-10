8b2de7e674ed99cc169d986df4700a2a7ddfd211
Byron González <byrong.gt@gmail.com>
2026-09-09 19:21:06 -0600

    actualizado dcl pino

--- dcl/altura_pino.r
+++ dcl/altura_pino.r
@@ -8,4 +8,22 @@ if(!require(readxl)){install.packages("readxl")}
 pino<-read_excel("alturapino.xlsx")
 
 head(pino)
+
+# Análisis del cuadrado latino
 with(pino,DQL(trat,pendiente,esolar,altura,mcomp = "tukey"))
+
+# Tukey para tratamientos, filas (pendiente) y columnas (esolar)
+pino$trat <- factor(pino$trat)
+pino$pendiente <- factor(pino$pendiente)
+pino$esolar <- factor(pino$esolar)
+
+modelo_dcl <- aov(altura ~ trat + pendiente + esolar, data = pino)
+summary(modelo_dcl)
+
+tukey_trat <- TukeyHSD(modelo_dcl, "trat")
+tukey_pendiente <- TukeyHSD(modelo_dcl, "pendiente")
+tukey_esolar <- TukeyHSD(modelo_dcl, "esolar")
+
+tukey_trat
+tukey_pendiente
+tukey_esolar
