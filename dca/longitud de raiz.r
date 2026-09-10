42f5d59954547874c4879ab00edb7d0edc003d79
Byron González <byrong.gt@gmail.com>
2026-09-09 19:33:50 -0600

    actualizado dca raiz

--- dca/longitud de raiz.r
+++ dca/longitud de raiz.r
@@ -3,6 +3,7 @@
 
 if(!require(AgroR)){install.packages("AgroR")}
 if(!require(readxl)){install.packages("readxl")}
+if(!require(agricolae)){install.packages("agricolae")}
 
 raiz<-read_excel("data/lraiz.xlsx")
 head(raiz)
@@ -10,3 +11,11 @@ with(raiz,DIC(concentracion,longraiz,mcomp = "tukey"))
 medias <- aggregate(longraiz ~ concentracion, data = raiz, mean)
 medias_ordenadas <- medias[order(medias$longraiz, decreasing = TRUE), ]
 medias_ordenadas
+
+modelo <- aov(longraiz ~ concentracion, data = raiz)
+summary(modelo)
+
+tukey_agricolae <- HSD.test(modelo, "concentracion", group = TRUE, console = TRUE)
+tukey_agricolae$groups
+plot(tukey_agricolae, main = "Prueba de Tukey - Longitud de raíz")
+
