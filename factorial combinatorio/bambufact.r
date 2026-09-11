b075051b4a6f6132e1a8229be527bf2efc517a4d
Byron González <byrong.gt@gmail.com>
2026-09-11 06:42:05 -0600

    actualizado bambu combinatorio

--- factorial combinatorio/bambufact.r
+++ factorial combinatorio/bambufact.r
@@ -3,8 +3,24 @@
 
 if(!require(AgroR)){install.packages("AgroR")}
 if(!require(readxl)){install.packages("readxl")}
+if(!require(agricolae)){install.packages("agricolae")}
 
-bambu<-read_excel("alturabambu.xlsx")
+bambu<-read_excel("data/alturabambu.xlsx")
 head(bambu)
 
 with(bambu,FAT2DBC(espacio,edad_rizoma,rep,altura,mcomp = "sk"))
+
+bambu$espacio <- factor(bambu$espacio)
+bambu$edad_rizoma <- factor(bambu$edad_rizoma)
+bambu$rep <- factor(bambu$rep)
+
+modelo_anova <- aov(altura ~ espacio * edad_rizoma + rep, data = bambu)
+summary(modelo_anova)
+
+tukey_interaccion <- HSD.test(
+	modelo_anova,
+	c("espacio", "edad_rizoma"),
+	group = TRUE,
+	console = TRUE
+)
+tukey_interaccion$groups
